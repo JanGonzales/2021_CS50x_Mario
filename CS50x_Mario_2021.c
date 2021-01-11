76440fab59bc0bafdb972092a4c85d0216843c96
Jan Gonzales <j.gonzalesnreal@gmail.com>
2021-01-11 19:37:25 +0000

    Fixing bugs on gitignore to UTF8, adding while loop to mario to ask user if yhey want to play again

--- CS50x_Mario_2021.c
+++ CS50x_Mario_2021.c
@@ -2,37 +2,52 @@
 
 int main()
 {
-    int height;
-    int counter;
-    int pyramid;
-    int space;
-    int x = 1;
-    printf("Welcome to Mario World, let's build some pyramids\n");
-    do
+    int x = 0;
+    while (x != 1)
     {
-        printf("Please enter the height\n");
-        scanf("%i", &height);
-    } 
-    while (height <= 1);
-    
-    for (counter = 0; counter < height; counter++)
-    {
-        for (space = x; space < height; space++)
+        int height;
+        int counter;
+        int pyramid;
+        int space;
+        int var = 1;
+        char play;
+        printf("\nWelcome to Mario World, let's build some pyramids\n");
+        do
         {
-            printf(" ");
-        }
-        for (pyramid = 0; pyramid <= counter; pyramid++)
-        {
-            printf("#");
-        }
-        printf("  ");
-        for (pyramid = 0; pyramid <= counter; pyramid++)
-        {
-            printf("#");
-        }
-        printf("\n");
-        pyramid = 0;
-        x += 1;
+            printf("Please enter the height\n");
+            scanf("%i", &height);
+        } 
+        while (height <= 1 || height > 8);
+
+            for (counter = 0; counter < height; counter++)
+            {
+                for (space = var; space < height; space++)
+                {
+                    printf(" ");
+                }
+                for (pyramid = 0; pyramid <= counter; pyramid++)
+                {
+                    printf("#");
+                }
+                printf("  ");
+                for (pyramid = 0; pyramid <= counter; pyramid++)
+                {
+                    printf("#");
+                }
+                printf("\n");
+                var += 1; // Place holder to subtract the number of spaces against the blocks. e.g var = 1 heights = 6, so for every blocks the spaces should 6 - 1 = space 
+            }
+            printf("Do you want to continue? y/N\n ");
+            scanf("%s", &play);
+            if (play == 'n' || play == 'N')
+            {
+                x++;
+            }
+            else
+            {
+                var = 1;
+            } 
     }
-    // printf("%i", height);
+
+
 }
